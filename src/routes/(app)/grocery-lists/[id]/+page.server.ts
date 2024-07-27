962517e550d90bacab4f3a4235ae95fc64d72c8b
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-07-27 10:39:38 -0400

    add ability to delete grocery lists

--- src/routes/(app)/grocery-lists/[id]/+page.server.ts
+++ src/routes/(app)/grocery-lists/[id]/+page.server.ts
@@ -1,8 +1,9 @@
 import { getUserFromSession } from '$lib/services/userSessions';
-import { error, redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 import { APIError } from '$lib/types/errors';
-import { getGroceryListById } from '$lib/services/groceryLists';
+import { deleteGroceryList, getGroceryListById } from '$lib/services/groceryLists';
+import { z } from 'zod';
 
 export const load: PageServerLoad = async ({ cookies, params }) => {
 	const user = await getUserFromSession(cookies);
@@ -30,3 +31,42 @@ export const load: PageServerLoad = async ({ cookies, params }) => {
 		});
 	}
 };
+
+export const actions = {
+	delete: async ({ cookies, params }) => {
+		const user = await getUserFromSession(cookies);
+		if (!user) {
+			redirect(307, '/auth');
+		}
+
+		const idRes = z.string().uuid().safeParse(params.id);
+		if (idRes.error) {
+			return fail(400, {
+				message: idRes.error.errors.map((err) => err.message).join(',')
+			});
+		}
+
+		try {
+			// will validate that the grocery list exists and that user has access
+			await getGroceryListById(user.id, idRes.data);
+
+			await deleteGroceryList(user.id, idRes.data);
+		} catch (e: unknown) {
+			if (e instanceof APIError) {
+				return fail(e.code, {
+					message: e.message
+				});
+			}
+
+			if (e instanceof Error) {
+				console.error(`error deleting grocery lsit: ${e.message}`);
+			}
+
+			return fail(500, {
+				message: 'Internal Server Error'
+			});
+		}
+
+		redirect(302, '/');
+	}
+} satisfies Actions;
